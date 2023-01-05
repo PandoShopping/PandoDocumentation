d8aee8181e09ec56c2c1b1d1ec5c8396be2df4ac
Vidushi <meelvidushi1@gmail.com>
2023-01-04 19:32:01 -0500

    Initial Commit

---
+++ Pando frontend/Pando_frontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pando_frontendApp.swift
+//  Pando frontend
+//
+//  Created by Vidushi Meel on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct Pando_frontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
